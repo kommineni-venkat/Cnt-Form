7b91d41bf6e090bb583de479adc61f49b39139a9
kommineni-venkat <komminenivenkat101@gmail.com>
2024-06-06 10:28:52 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import ContactForm from './ContactForm';
 
-import Image from './images/doctor.jpg';
+import  './images/doctor.jpg';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Contact Us</h1>
         <ContactForm />
-        <img src="./images/doctor.jpg" alt="image" className="image" />
+        <img src="./images/doctor.jpg" alt="image"/>
       </header>
     </div>
   );
